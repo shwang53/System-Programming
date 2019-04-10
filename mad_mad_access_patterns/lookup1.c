0be0cb3046934bacd05879e804325252112f0232
Suk Min Hwang <shwang53@illinois.edu>
2019-04-10 18:16:49 -0500

    file submission

--- mad_mad_access_patterns/lookup1.c
+++ mad_mad_access_patterns/lookup1.c
@@ -4,6 +4,12 @@
  */
 #include "tree.h"
 #include "utils.h"
+
+#include <stdlib.h>
+#include <string.h>
+
+#define BTNSIZE (sizeof(BinaryTreeNode))
+
 /*
   Look up a few nodes in the tree and print the info they contain.
   This version uses fseek() and fread() to access the data.
@@ -11,6 +17,82 @@
   ./lookup1 <data_file> <word> [<word> ...]
 */
 
+static uint32_t node_offset;
+static int is_found;
+
+static void search(FILE * file, uint32_t offset, char * word);
+
 int main(int argc, char **argv) {
+    if (argc < 3) {
+        printArgumentUsage();
+        exit(1);
+    }
+
+    FILE * data_file = fopen(argv[1], "r");
+    if (!data_file) {
+        openFail(argv[1]);
+        exit(2);
+    }
+    fseek(data_file, 0, SEEK_SET);
+    char * buf = malloc(4 * sizeof(char) + 1);
+    fread(buf, sizeof(char), 4, data_file);
+    buf[4] = '\0';
+    if (strcmp(buf, BINTREE_HEADER_STRING)) {
+        formatFail(argv[1]);
+        exit(2);
+    }
+    free(buf);
+
+    for (int i = 2; i < argc; i++) {
+        char * word = argv[i];
+        node_offset = 0;
+        is_found = 0;
+        search(data_file, BINTREE_ROOT_NODE_OFFSET, word);
+        if (is_found) {
+            fseek(data_file, node_offset, SEEK_SET);
+            BinaryTreeNode * found = malloc(BTNSIZE);
+            fread(found, BTNSIZE, 1, data_file);
+            printFound(word, found->count, found->price);
+            free(found);
+        } else {
+            printNotFound(word);
+        }
+    }
+
+    fclose(data_file);
     return 0;
 }
+
+static void search(FILE * file, uint32_t offset, char * word) {
+    fseek(file, offset + 16, SEEK_SET);
+    char * buf = malloc(256);
+    size_t n = 0;
+    int c;
+    c = fgetc(file);
+    while (c != EOF && c != '\0') {
+        buf[n++] = (char)c;
+        c = fgetc(file);
+    }
+    buf[n] = '\0';
+    int result = strcmp(word, buf);
+    if (!result) {
+        is_found = 1;
+        node_offset = offset;
+        free(buf);
+        return;
+    }
+    free(buf);
+
+    fseek(file, offset, SEEK_SET);
+    BinaryTreeNode * node = malloc(BTNSIZE);
+    fread(node, BTNSIZE, 1, file);
+    if (node->left_child && result < 0 && !is_found) {
+        search(file, node->left_child, word);
+    }
+    if (node->right_child && result > 0 && !is_found) {
+        search(file, node->right_child, word);
+    }
+    free(node);
+}
+
+
--- mad_mad_access_patterns/lookup2.c
+++ mad_mad_access_patterns/lookup2.c
@@ -5,6 +5,14 @@
 #include "tree.h"
 #include "utils.h"
 
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/mman.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
 /*
   Look up a few nodes in the tree and print the info they contain.
   This version uses mmap to access the data.
@@ -12,6 +20,80 @@
   ./lookup2 <data_file> <word> [<word> ...]
 */
 
+static void * addr;
+static uint32_t node_offset;
+static int is_found;
+
+static void search(uint32_t offset, char * word);
 int main(int argc, char **argv) {
-    return 0;
+    
+    if (argc < 3) {
+        printArgumentUsage();
+        exit(1);
+    }
+
+    int fd = open(argv[1], O_RDONLY);
+    if (fd == -1) {
+        openFail(argv[1]);
+        exit(2);
+    }
+
+    int file_size = lseek(fd, 0, SEEK_END);
+    addr = mmap(NULL, file_size, PROT_READ, MAP_PRIVATE, fd, 0);
+    if (addr == (void *)-1) {
+        mmapFail(argv[1]);
+        exit(2);
+    }
+
+    char * temp = addr;
+    if (strncmp(temp, BINTREE_HEADER_STRING, 4)) {
+        formatFail(argv[1]);
+        exit(2);
+    }
+
+    for (int i = 2; i < argc; i++) {
+        char * word = argv[i];
+        node_offset = 0;
+        is_found = 0;
+        search(BINTREE_ROOT_NODE_OFFSET, word);
+        if (is_found) {
+            BinaryTreeNode * found = addr + node_offset;
+            printFound(word, found->count, found->price);
+        } else {
+            printNotFound(word);
+        }
+    }
+
+    munmap(addr, file_size);
+    close(fd);
+     
+   return 0;
+}
+
+static void search(uint32_t offset, char * word) {
+    char * temp = addr + offset + 16;
+    char * buf = malloc(256);
+    size_t n = 0;
+    while (temp[n] != '\0') {
+        buf[n] = temp[n];
+        n++;
+    }
+    buf[n] = '\0';
+    int result = strcmp(word, buf);
+    if (!result) {
+        is_found = 1;
+        node_offset = offset;
+        free(buf);
+        return;
+    }
+    free(buf);
+
+    uint32_t * bt_array = addr + offset;
+    uint32_t left = bt_array[0], right = bt_array[1];
+    if (left && result < 0 && !is_found) {
+        search(left, word);
+    }
+    if (right && result > 0 && !is_found) {
+        search(right, word);
+    }
 }
